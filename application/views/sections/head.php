27488bb92c229b01ffef03a353ddbd4c0b5dcb2e
fazhal <fazhaldarul@gmail.com>
2015-06-27 00:01:33 +0700

    fixed forgot

--- application/views/sections/head.php
+++ application/views/sections/head.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Foundation | Welcome</title>
+    <title>Lailatul Coding</title>
     <link rel="stylesheet" href="<?php echo base_url('assets/foundation'); ?>/stylesheets/foundation.css" />
     <script src="<?php echo base_url('assets/foundation'); ?>/javascripts/modernizr.js"></script>
   </head>

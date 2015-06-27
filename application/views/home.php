0461d29cf0e76d3462b9e39bc73ceb28c569899a
Angga Nugraha <annu.nura@gmail.com>
2015-06-27 02:45:01 -0700

    update home
    halaman utama ada di view->home.php

--- application/views/home.php
+++ application/views/home.php
@@ -13,19 +13,55 @@
 <!-- Header Navigation-->
 <div class="content">
 	<div class="row">
-	<img src="<?php echo base_url('assets/images'); ?>/header-background.jpg" />
-		<div class="head-nav navi">
-			<div class="left top-20">
-				<img src="<?php echo base_url('assets/images'); ?>/logo-lailatul-coding.png" />
+			<div class="head-nav">
+				<div class="left top-20">
+					<img src="<?php echo base_url('assets/images'); ?>/logo-lailatul-coding.png" />
+				</div>
+				<div class="right top-30">
+					<a href="" class="small button ">Cara Kerja</a>
+					<a href="" class="small button ">Registrasi</a>
+					<a href="" class="small button ">Laporan</a>
+				</div>
 			</div>
-			<div class="right top-30">
-				<a href="" class="small button ">Cara Kerja</a>
-				<a href="" class="small button ">Registrasi</a>
-				<a href="" class="small button ">Laporan</a>
+			<div class="caption-banner">
+				<p>Malam Lailatul Coding adalah malam amal bagi para pekerja kreatif <br>
+				untuk saling berpartisipasi tidak hanya dalam bentuk uang, tetapi <br>
+				juga berupa sumbangan keahlian.</p>
+				<a href="" class="small button ">Lailatul Coding 2016, 1 juni - 2 juni, 18:00-05:00</a>
+			</div>
+		<img src="<?php echo base_url('assets/images'); ?>/header-background.jpg" />
+	</div>
+	<div class="row">
+			<h1 class="center">Cara Kerja</h1>
+		<div class="center">
+			<div class="small-4 column" style="background:#000;">
+				<img src="<?php echo base_url('assets/images'); ?>/icon-developer.png" />
+				<h3>Developer / Designer</h3>
+				<p>Sumbangan dapat berbentuk keahlian Coding / Design untuk menyelesaikan project</p>
+			</div>
+			<div class="small-4 column" style="background:#000;">
+				<img src="<?php echo base_url('assets/images'); ?>/icon-project-owner.png" />
+				<h3>Project Owner</h3>
+				<p>Sewa Developer atau Designer ditempat untuk menyumbang</p>
+			</div>
+			<div class="small-4 column" style="background:#000;">
+				<img src="<?php echo base_url('assets/images'); ?>/icon-donation.png" />
+				<h3>Donate On The Spot</h3>
+				<p>Datang Langsung untuk memberikan sumbangan</p>
+			</div>
+		</div>
+		<div class="row">
+			
+			<img src="<?php echo base_url('assets/images'); ?>/donation-background.png" width="100%"/>
+			<div class="caption-flag">
+				<p>Lailatul Coding 2016 akan disumbangkan <br>
+				kepada Red Cross International. <br>
+				www.redcross.com</p>
 			</div>
 		</div>
 	</div>
 </div>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <!-- End Header Navigation-->
 
 	<script src="<?php echo base_url('assets/foundation'); ?>/javascripts/jquery.min.js"></script>
